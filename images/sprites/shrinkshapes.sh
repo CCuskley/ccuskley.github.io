0351a5efe0c8ae517767807fce8865138ac8f4d8
CCuskley <ccuskley@gmail.com>
2020-03-26 15:12:33 +0000

    reverse

---
+++ images/sprites/shrinkshapes.sh
@@ -0,0 +1,15 @@
+convert cell-1.png -resize 15% cell-1-sm.png
+convert cell-2.png -resize 15% cell-2-sm.png
+convert cell-3.png -resize 15% cell-3-sm.png
+convert cell-4.png -resize 15% cell-4-sm.png
+convert cell-5.png -resize 15% cell-5-sm.png
+convert sickcell-5.png -resize 15% sickcell-5-sm.png
+convert sickcell-4.png -resize 15% sickcell-4-sm.png
+convert sickcell-3.png -resize 15% sickcell-3-sm.png
+convert sickcell-2.png -resize 15% sickcell-2-sm.png
+convert sickcell-1.png -resize 15% sickcell-1-sm.png
+convert infectedcell-1.png -resize 15% infectedcell-1-sm.png
+convert infectedcell-2.png -resize 15% infectedcell-2-sm.png
+convert infectedcell-3.png -resize 15% infectedcell-3-sm.png
+convert infectedcell-4.png -resize 15% infectedcell-4-sm.png
+convert infectedcell-5.png -resize 15% infectedcell-5-sm.png
