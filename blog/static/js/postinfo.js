7f556cf88090128763875723ca60b96732a95152
Christine Cuskley <ccuskley@gmail.com>
2025-03-21 14:38:00 +0000

    writing slides

---
+++ blog/static/js/postinfo.js
@@ -0,0 +1,11 @@
+var posts = [
+
+	{	
+		"date":"November 2024",
+		"title":"The rise and fall of digital literacy",
+		"lede":"How curricular reforms gutted basic computer skills, and why it won't get better anytime soon.",
+		"image":"images/computer.jpg",
+		"tags":["teaching","programming","hottakes"]
+		}
+
+]
